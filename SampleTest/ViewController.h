7f2cb5c1cb99a486577ef7780fbaf397966de8c1
Nandini Tomke <Nandini@Nandinis-Mac-mini.local>
2012-09-24 11:56:49 +0530

    Adding Sample test

---
+++ SampleTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleTest
+//
+//  Created by Nandini Tomke on 20/09/12.
+//  Copyright (c) 2012 Nandini Tomke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SampleTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleTest
+//
+//  Created by Nandini Tomke on 20/09/12.
+//  Copyright (c) 2012 Nandini Tomke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
